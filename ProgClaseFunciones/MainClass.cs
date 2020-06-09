54e4381b02d034128575d7b81d3d2c496973a6ba
G <G@PC>
2020-06-08 19:21:08 -0500

    Agregar archivos de proyecto.

---
+++ ProgClaseFunciones/MainClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgClaseFunciones
+{
+    class MainClass
+    {
+        // funcion determinar si un numero es par o impar
+        // funcion que devuelveel nombre del mes previo un parametro ingresado 
+        public void fnNombreMes (int mes)
+        {
+
+        }
+    }
+}
